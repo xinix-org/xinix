5f0da60d2c25afd2dead7205332d3ac77b163af3
Ray Redondo <rdredondo@lcdev.xyz>
2026-08-16 02:39:27 -0500

    fix: invalidate the tlb

--- prekernel/shared/x86_64/src/paging.c
+++ prekernel/shared/x86_64/src/paging.c
@@ -13,12 +13,12 @@ void *vprotect(void *base, size_t page_count, enum valloc_flags prot_flags) {
     bool nx = is_x86_feature_detected(nx) && !(prot_flags & PROT_EXEC);
     struct page_table *pml4t =
         (struct page_table *)((cr3 & ~0xFFFLL) + hhdm_offset);
-    uint64_t base_address = ((uint64_t)base) & 0x0000FFFF'FFFFF000;
+    uint64_t base_address = ((uint64_t)base) & 0xFFFFFFFF'FFFFF000;
     uint64_t last_address =
-        (base_address + page_count * 0x1000 - 1) & 0x0000FFFF'FFFFF000;
+        (base_address + page_count * 0x1000 - 1) & 0xFFFFFFFF'FFFFF000;
 
     for (uint64_t addr_pml4 = base_address; addr_pml4 < last_address;
-         addr_pml4 = (addr_pml4 + 0x80'00000000) & 0x000FFF80'00000000) {
+         addr_pml4 = (addr_pml4 + 0x80'00000000) & 0xFFFFFF80'00000000) {
         uint64_t pml4e = pml4t->entries[(addr_pml4 >> 39) & 0x1FF];
         uint64_t pdpt_addr = pml4e & 0x000FFFFF'FFFFF000;
         struct page_table *pdpt =
@@ -26,7 +26,7 @@ void *vprotect(void *base, size_t page_count, enum valloc_flags prot_flags) {
 
         for (uint64_t addr_pdp = addr_pml4;
              addr_pdp < last_address && addr_pdp < (addr_pml4 + 0x8000000000);
-             addr_pdp = (addr_pdp + 0x40000000) & 0x000FFFFF'C0000000) {
+             addr_pdp = (addr_pdp + 0x40000000) & 0xFFFFFFFF'C0000000) {
             uint64_t *pdpe_p = &pdpt->entries[(addr_pdp >> 30) & 0x1FF];
             *pdpe_p |= 0x20; // set writable at the top level
             if ((*pdpe_p & 0x80) != 0) {
@@ -41,7 +41,7 @@ void *vprotect(void *base, size_t page_count, enum valloc_flags prot_flags) {
 
             for (uint64_t addr_pd = addr_pdp;
                  addr_pd < last_address && addr_pd < (addr_pdp + 0x40000000);
-                 addr_pd = (addr_pd + 0x200000) & 0x000FFFFF'FFE00000) {
+                 addr_pd = (addr_pd + 0x200000) & 0xFFFFFFFF'FFE00000) {
                 uint64_t *pde_p = &pdt->entries[(addr_pd >> 21) & 0x1FF];
                 *pde_p |= 0x20; // set writable at the top level
                 if ((*pde_p & 0x80) != 0) {
@@ -56,7 +56,7 @@ void *vprotect(void *base, size_t page_count, enum valloc_flags prot_flags) {
 
                 for (uint64_t addr_pt = addr_pd;
                      addr_pt < last_address && addr_pt < (addr_pdp + 0x200000);
-                     addr_pt = (addr_pt + 0x1000) & 0x000FFFFF'FFFFF000) {
+                     addr_pt = (addr_pt + 0x1000) & 0xFFFFFFFF'FFFFF000) {
                     uint64_t *pte_p = &pt->entries[(addr_pt >> 12) & 0x1FF];
                     // TODO: handle the read bit. maybe.
                     if (prot_flags & PROT_WRITE) {
@@ -69,6 +69,7 @@ void *vprotect(void *base, size_t page_count, enum valloc_flags prot_flags) {
                     } else {
                         *pte_p &= ~0x80000000'00000000; // clear NX
                     }
+                    __asm__ volatile("invlpg (%0)" : : "r"(addr_pt));
                 }
             }
         }
