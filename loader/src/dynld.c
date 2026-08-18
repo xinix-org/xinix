f0de4a5d338464ef5cf056c26db22cac8c93638b
Connor Horman <chorman64@gmail.com>
2026-08-17 20:21:34 -0400

    I hate C

--- loader/src/dynld.c
+++ loader/src/dynld.c
@@ -424,7 +424,7 @@ next:
             } break;
             case DT_RELA: {
                 DO_REL(ent.dylib_plt_rela, addend_rela, false, jmprel_defered);
-            }
+            } break;
             default:
                 return SYSRESULT2_ERROR(-999);
             }
