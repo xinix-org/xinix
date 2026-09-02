32fc8f833fce5e43f96312381fb6d90d3c931750
Connor Horman <chorman64@gmail.com>
2026-09-02 00:55:04 -0400

    Add other event constructors and document them properly

--- kernel/include/event.h
+++ kernel/include/event.h
@@ -7,11 +7,22 @@ enum EventKinds : uint32_t {
     EVENT_MOUSE_BUTTON = 0x02'00'0000,
     EVENT_BUTTON_DOWN = 0x00'01'0000,
     EVENT_BUTTON_UP = 0x00'02'00000,
-    EVENT_MOUSE_MOTION = 0x03'000000,
+    EVENT_MOUSE_MOTION = 0x03'000'000,
     EVENT_TIMER = 0x04'000000,
 };
 
-#define KEY(step, scode)
+/// An event word for a key press
+#define KEY(scode) (EVENT_KEY | (scode))
 
-/// Writes `r_event` to the
+/// An event word for a mouse button press
+#define MOUSE_BUTTON(step, button) (EVENT_MOUSE_BUTTON | (step) | (button))
+
+/// An event word for a mouse motion input
+#define MOUSE_MOTION(dx, dy) (EVENT_MOUSE_MOTION | ((dx) << 12) | (dy))
+
+/// An event word for the timer interrupt
+#define TIMER(dt) (EVENT_TIMER | (dt))
+
+/// Writes `r_event` to the event buffer.
+/// Event should be constructed using `KEY`, `MOUSE_BUTTON`, `MOUSE_MOTION`, or TIMER`
 void push_event(uint32_t r_event);
